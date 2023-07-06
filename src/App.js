ab37fdecffbb2bf93a091c004c2af611b471d8c9
Juan Alejandro Martinez Linares <alej1286@gmail.com>
2023-07-06 18:51:14 -0400

    updated Node using nvm, added amplify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@aws-amplify/ui-react/styles.css";
+
 import SiteFooter from "./components/Common/SiteFooter";
 import SiteNav from "./components/Common/SiteNav";
 import HomePage from "./components/home/HomePage";
@@ -8,19 +15,23 @@ import LoginPage from "./components/auth/LoginPage";
 import RegisterPage from "./components/auth/RegisterPage";
 import Contacts from "./components/contacts/Contacts";
 
+Amplify.configure(awsExports);
+
 function App() {
   return (
-    <div>
-      <SiteNav />
-      <Routes>
-        <Route path="*" element={<HomePage />} />
-        <Route path="/" exact={true} element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
-      <SiteFooter />
-    </div>
+    <Authenticator>
+      <div>
+        <SiteNav />
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+          <Route path="/" exact={true} element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <SiteFooter />
+      </div>
+    </Authenticator>
   );
 }
 
