57c909c3bd1da170d46e0523f03b0358143b4765
Juan Alejandro Martinez Linares <alej1286@gmail.com>
2023-07-07 18:27:45 -0400

    list contacts using graphql from s3 and amplify

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import { API, graphqlOperation, Storage } from "aws-amplify";
 import { createContact } from "../../graphql/mutations";
+import { listContacts } from "../../graphql/queries";
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,29 +13,61 @@ import Button from "react-bootstrap/Button";
 import { v4 as uuid } from "uuid";
 
 export default function Contacts(props) {
+  const [contacts, setContacts] = useState([]);
   const [contactData, setContactData] = useState({
     name: "",
     email: "",
     cell: "",
   });
   const [profilePic, setProfilePic] = useState("");
+  const [profilePicPaths, setProfilePicPaths] = useState([]);
+
+  const getContacts = async () => {
+    try {
+      const contactData = await API.graphql(graphqlOperation(listContacts));
+      console.log(contactData);
+      const contactList = contactData.data.listContacts.items;
+      setContacts(contactList);
+      contacts.map(async (contact, indx) => {
+        const contactProfilePicPath = contacts[indx].profilePicPath;
+        try {
+          const contactProfilePicPathURI = await Storage.get(
+            contactProfilePicPath,
+            { expires: 60 }
+          );
+          setProfilePicPaths([...profilePicPaths, contactProfilePicPathURI]);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
 
   const addNewContact = async () => {
     const { name, email, cell } = contactData;
+    try {
+      Storage.configure({ region: "us-east-1" });
+      const { key } = await Storage.put(`${uuid()}.png`, profilePic, {
+        contentType: "image/png",
+      });
 
-    Storage.configure({ region: "us-east-1" });
-    const { key } = await Storage.put(`${uuid()}.png`, profilePic, {
-      contentType: "image/png",
-    });
-
-    const newContact = {
-      id: uuid(),
-      name,
-      email,
-      cell,
-      profilePicPath: key,
-    };
-    await API.graphql(graphqlOperation(createContact, { input: newContact }));
+      const newContact = {
+        id: uuid(),
+        name,
+        email,
+        cell,
+        profilePicPath: key,
+      };
+      await API.graphql(graphqlOperation(createContact, { input: newContact }));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -43,20 +77,24 @@ export default function Contacts(props) {
           <h1>Contacts</h1>
         </Col>
       </Row>
-      <Row className="px-4 my-5">
-        <Col className="px-2 my-2">
-          <Card style={{ width: "12rem" }}>
-            <Card.Img src="image/contact_1.png" variant="top" />
-            <Card.Body>
-              <Card.Title>Stacy</Card.Title>
-              <Card.Text>
-                Stacy
-                <br />
-                786.234.543
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+      <Row>
+        {contacts.map((contact, indx) => {
+          return (
+            <Col className="px-2 my-2" key={indx}>
+              <Card style={{ width: "12rem" }}>
+                <Card.Img src={profilePicPaths[indx]} variant="top" />
+                <Card.Body>
+                  <Card.Title>{contact.name}</Card.Title>
+                  <Card.Text>
+                    {contact.email}
+                    <br />
+                    {contact.cell}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
       <Row className="px-4 my-5">
         <Col sm={3}>
